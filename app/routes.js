799130b51de5955500c57b9f82a478c8fc5a02eb
Ian M. Dawson <ianmdawson@gmail.com>
2017-11-25 21:52:21 -0800

    clean up routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,6 @@
 // app/routes.js
 'use strict'
 
-var moment = require('moment')
-var Promise = require('bluebird')
-var join = Promise.join
-
 module.exports = function(app) {
   app.get('/', function(req, res) {
     res.render('index', {})
